c3cb5b37607463cc42069fd7c4e695d5627a21ce
Joey Flores <61069122+Coalemus@users.noreply.github.com>
2022-05-29 21:00:41 +0800

    Create wofifilter.sh

---
+++ wofifilter.sh
@@ -0,0 +1,15 @@
+# Automatically filters unused desktop icons to filter wofi.
+
+cd /usr/share/applications/
+
+# sudo mv .desktop ~/desktopfilter/
+
+sudo mv thunar-settings.desktop ~/desktopfilter/
+sudo mv thunar-bulk-rename.desktop ~/desktopfilter/
+sudo mv electron.desktop ~/desktopfilter/
+sudo mv electron15.desktop ~/desktopfilter/
+sudo mv electron17.desktop ~/desktopfilter/
+sudo mv cups.desktop ~/desktopfilter/
+sudo mv calibre-ebook-edit.desktop ~/desktopfilter/
+sudo mv calibre-ebook-viewer.desktop ~/desktopfilter/
+sudo mv calibre-lrfviewer.desktop ~/desktopfilter/
